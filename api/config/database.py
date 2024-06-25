2a7954c9ffe3503c63235d82e039476766e988da
Christian Keller <k.christian9@web.de>
2024-06-25 23:35:56 +0200

    feat(database): using sqllite for development

--- api/config/database.py
+++ api/config/database.py
@@ -3,33 +3,31 @@ from typing import Annotated
 from dotenv import load_dotenv
 from fastapi.params import Depends
 from sqlalchemy import create_engine
-from api.models.database import models
 from sqlalchemy.orm import sessionmaker, Session
-
+from api.models.database import models
 
 load_dotenv()
-LOCAL_DB        = os.getenv("LOCAL_DB")
+LOCAL_DB = os.getenv("LOCAL_DB")
 
-MYSQL_HOST      = os.getenv("MYSQL_HOST")       if not LOCAL_DB else os.getenv("MYSQL_HOST_LOCAL")
-MYSQL_USER      = os.getenv("MYSQL_USER")       if not LOCAL_DB else os.getenv("MYSQL_USER_LOCAL")
-MYSQL_PASSWORD  = os.getenv("MYSQL_PASSWORD")   if not LOCAL_DB else os.getenv("MYSQL_PASSWORD_LOCAL")
-MYSQL_DB        = os.getenv("MYSQL_DB")         if not LOCAL_DB else os.getenv("MYSQL_DB_LOCAL")
+if LOCAL_DB:
+    SQLITE_DB_PATH = os.getenv("SQLITE_DB_PATH")
+    URL_DATABASE = f"sqlite:///{SQLITE_DB_PATH}"
+else:
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB = os.getenv("MYSQL_DB")
+    URL_DATABASE = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
 
-URL_DATABASE = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
-engine = create_engine(URL_DATABASE)
+engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False} if LOCAL_DB else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 models.Base.metadata.create_all(bind=engine)
 
-
 def get_db():
     db = SessionLocal()
-    try: 
+    try:
         yield db
     finally:
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
- 
-
-
-
