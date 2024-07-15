9fb3503cc4da21f6965e18a9848d54ed34a9893a
Christian99-dev <k.christian9@web.de>
2024-07-15 12:15:52 +0200

    refactor: songs response .from_orm()

--- api/models/routes/songs.py
+++ api/models/routes/songs.py
@@ -1,27 +1,21 @@
 from pydantic import BaseModel
 
-
-# abstractions
-class Song(BaseModel):
-    song_id: int
-    name: str
-    author: str
-    times_used: int
-    cover_src: str
-    audio_src: str
-
-    class Config:
-        from_attributes = True
-
 # /create
 class CreateRequest(BaseModel):
     name: str
     author: str
     cover_src: str
     audio_src: str
-
+    
 class CreateResponse(BaseModel):
-    message: str
-    song: Song
+    song_id: int
+    name: str
+    author: str
+    times_used: int
+    cover_src: str
+    audio_src: str
+    
+    class Config:
+        from_attributes = True
 
 
