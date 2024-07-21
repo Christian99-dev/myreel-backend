826910faa3ac7d8ac464e30752430978dc69c175
Christian99-dev <k.christian9@web.de>
2024-07-21 15:30:14 +0200

    fix: real empty test client and better config for test database

--- test/conftest.py
+++ test/conftest.py
@@ -1,28 +1,33 @@
-import logging
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, StaticPool
 from sqlalchemy.orm import sessionmaker
 from api.models.database.model import Base
 from api.utils.database.fill_test_model import fill_test_model
 from logging_config import setup_logging_testing
-from ..main import app
+from main import app
 from api.config.database import get_db
 
 # setup logging
 setup_logging_testing()
 
 # setup test db
-engine = create_engine("sqlite:///:memory:")
+engine = create_engine("sqlite:///:memory:", 
+    connect_args={"check_same_thread": False,},
+    poolclass=StaticPool
+)
+
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# ONLY DATABASE
+# DB TABLES 
 @pytest.fixture(scope="session")
 def db_engine():
     Base.metadata.create_all(bind=engine)
     yield engine
     Base.metadata.drop_all(bind=engine)
 
+# ONLY DATABASE
 @pytest.fixture(scope="function")
 def db_session_empty(db_engine):
     """Creates a new database session for a test."""
@@ -35,6 +40,7 @@ def db_session_empty(db_engine):
     session.close()
     transaction.rollback()
     connection.close()
+
 @pytest.fixture(scope="function")
 def db_session_filled(db_engine):
     """Creates a new database session for a test."""
@@ -51,19 +57,10 @@ def db_session_filled(db_engine):
 
 # FULL APP CLIENT
 @pytest.fixture(scope="function")
-def app_client(db_engine):
-    def override_get_db_empty():
-        db = TestingSessionLocal()
-        try:
-            yield db
-        finally:
-            db.close()
-            
-    app.dependency_overrides[get_db] = override_get_db_empty
-    yield TestClient(app)
-    app.dependency_overrides.pop(get_db, None)
+def app_client_empty(db_engine):
+    empty_app = FastAPI()        
+    yield TestClient(empty_app)
 
-    
 @pytest.fixture(scope="function")
 def app_client_filled(db_engine):
     def override_get_db_filled():
@@ -73,3 +70,7 @@ def app_client_filled(db_engine):
             yield db
         finally:
             db.close()
+            
+    app.dependency_overrides[get_db] = override_get_db_filled
+    yield TestClient(app)
+    app.dependency_overrides.pop(get_db, None)
