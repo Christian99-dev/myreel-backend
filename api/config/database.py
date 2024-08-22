ebc955101459156a395d61ff012f60b3cc78d6e5
Christian99-dev <k.christian9@web.de>
2024-08-22 23:41:59 +0200

    refactor: revert last one

--- api/config/database.py
+++ api/config/database.py
@@ -10,14 +10,19 @@ from test.utils.testing_data.db.fill import fill
 from api.utils.database.print_database_contents import print_database_contents
 from distutils.util import strtobool
 logger = logging.getLogger("testing")
-load_dotenv()
 
-# generate database url
+load_dotenv()
 LOCAL_DB = strtobool(os.getenv("LOCAL_DB"))
+
 if LOCAL_DB:
     URL_DATABASE    = f"sqlite:///./local.db"
 else:
-    URL_DATABASE    = f"mysql+pymysql://{os.getenv("MYSQL_USER")}:{os.getenv("MYSQL_PASSWORD")}@{os.getenv("MYSQL_HOST")}/{os.getenv("MYSQL_DB")}"
+    MYSQL_HOST      = os.getenv("MYSQL_HOST")
+    MYSQL_USER      = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD  = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB        = os.getenv("MYSQL_DB")
+    
+    URL_DATABASE    = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
 
 # Start con
 engine  = create_engine(URL_DATABASE, connect_args={"check_same_thread": False} if LOCAL_DB else {})
