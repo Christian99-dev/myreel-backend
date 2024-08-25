ead1679a66d29e53a70ea9c09163f610ed4799ea
Christian99-dev <k.christian9@web.de>
2024-08-25 18:19:02 +0200

    refactor(test): role using memory db

--- test/api/auth/test_role.py
+++ test/api/auth/test_role.py
@@ -46,45 +46,45 @@ def test_role_has_access_methode_without_include_sub_roles():
     role_instance._role = RoleEnum.EXTERNAL
     role_tester_has_access(role_instance,  RoleEnum.EXTERNAL)
 
-def test_role_external(db_session_filled: Session):
+def test_role_external(db_memory: Session):
     # just wrong inputs
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,       userid=None,    groupid=None,   editid=None),    db_session=db_session_filled),  RoleEnum.EXTERNAL)    
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=-99,     groupid="-99",    editid=-99),     db_session=db_session_filled),  RoleEnum.EXTERNAL)
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,       userid=None,    groupid=None,   editid=None),    db_session=db_memory),  RoleEnum.EXTERNAL)    
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=-99,     groupid="-99",    editid=-99),     db_session=db_memory),  RoleEnum.EXTERNAL)
 
     # no user given
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=-99,     groupid=group_id_1,      editid=1),        db_session=db_session_filled),  RoleEnum.EXTERNAL)
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=None,    groupid=group_id_1,      editid=1),        db_session=db_session_filled),  RoleEnum.EXTERNAL)
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=-99,     groupid=group_id_1,      editid=1),        db_session=db_memory),  RoleEnum.EXTERNAL)
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=None,    groupid=group_id_1,      editid=1),        db_session=db_memory),  RoleEnum.EXTERNAL)
     
     # wrong group    
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=4,     groupid=group_id_1,    editid=1),        db_session=db_session_filled),  RoleEnum.EXTERNAL)
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=4,     groupid=group_id_1,    editid=None),     db_session=db_session_filled),  RoleEnum.EXTERNAL)
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=5,     groupid=group_id_1,    editid=None),     db_session=db_session_filled),  RoleEnum.EXTERNAL)
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=4,     groupid=group_id_1,    editid=1),        db_session=db_memory),  RoleEnum.EXTERNAL)
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=4,     groupid=group_id_1,    editid=None),     db_session=db_memory),  RoleEnum.EXTERNAL)
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=5,     groupid=group_id_1,    editid=None),     db_session=db_memory),  RoleEnum.EXTERNAL)
     
     # wrong edit    
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=3,     groupid=None,    editid=7),        db_session=db_session_filled),  RoleEnum.EXTERNAL)
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken="wrong",    userid=3,     groupid=None,    editid=7),        db_session=db_memory),  RoleEnum.EXTERNAL)
     
-def test_role_admin(db_session_filled: Session):
+def test_role_admin(db_memory: Session):
     admin_token = os.getenv("ADMIN_TOKEN")    
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=admin_token, userid=None,   groupid=None,   editid=None),    db_session=db_session_filled),   RoleEnum.ADMIN)
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=admin_token, userid=None,   groupid=None,   editid=None),    db_session=db_memory),   RoleEnum.ADMIN)
 
-def test_role_group_creator(db_session_filled: Session):
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,   userid=1,   groupid=group_id_1,  editid=None), db_session=db_session_filled), RoleEnum.GROUP_CREATOR)
+def test_role_group_creator(db_memory: Session):
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,   userid=1,   groupid=group_id_1,  editid=None), db_session=db_memory), RoleEnum.GROUP_CREATOR)
     
-def test_role_edit_creator(db_session_filled: Session):
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,   userid=1,   groupid=None,  editid=1), db_session=db_session_filled), RoleEnum.EDIT_CREATOR)
+def test_role_edit_creator(db_memory: Session):
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,   userid=1,   groupid=None,  editid=1), db_session=db_memory), RoleEnum.EDIT_CREATOR)
 
-def test_role_group_member(db_session_filled: Session):
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,   userid=2,   groupid=group_id_1,  editid=None), db_session=db_session_filled), RoleEnum.GROUP_MEMBER)   
+def test_role_group_member(db_memory: Session):
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,   userid=2,   groupid=group_id_1,  editid=None), db_session=db_memory), RoleEnum.GROUP_MEMBER)   
 
-def test_role_always_highest(db_session_filled: Session):  
+def test_role_always_highest(db_memory: Session):  
     admin_token = os.getenv("ADMIN_TOKEN")    
     
     # ADMIN 
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=admin_token,   userid=1,        groupid=group_id_1,      editid=None), db_session=db_session_filled), RoleEnum.ADMIN)  # ALSO GROUP_CREATOR
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=admin_token,   userid=1,        groupid=None,   editid=1),    db_session=db_session_filled), RoleEnum.ADMIN)  # ALSO EDIT_CREATOR
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=admin_token,   userid=2,        groupid=group_id_1,      editid=None), db_session=db_session_filled), RoleEnum.ADMIN)  # ALSO GROUP_MEMBER
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=admin_token,   userid=1,        groupid=group_id_1,      editid=None), db_session=db_memory), RoleEnum.ADMIN)  # ALSO GROUP_CREATOR
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=admin_token,   userid=1,        groupid=None,   editid=1),    db_session=db_memory), RoleEnum.ADMIN)  # ALSO EDIT_CREATOR
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=admin_token,   userid=2,        groupid=group_id_1,      editid=None), db_session=db_memory), RoleEnum.ADMIN)  # ALSO GROUP_MEMBER
     
     # GROUP_CREATOR 
-    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,           userid=1,        groupid=group_id_1,     editid=1),    db_session=db_session_filled), RoleEnum.GROUP_CREATOR)  # ALSO EDIT_CREATOR
+    role_tester_has_access(Role(role_infos=RoleInfos(admintoken=None,           userid=1,        groupid=group_id_1,     editid=1),    db_session=db_memory), RoleEnum.GROUP_CREATOR)  # ALSO EDIT_CREATOR
 
 
