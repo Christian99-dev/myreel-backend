c53e1207ae12c06aaa5eb4db62f848dc41a77aa1
Christian99-dev <k.christian9@web.de>
2024-08-25 20:58:55 +0200

    refactor(test): remove unused imports and fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -2,11 +2,10 @@ import pytest
 import logging
 from fastapi import Depends, FastAPI, Request
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine, StaticPool
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import Session
 from api.middleware.access_handler import AccessHandlerMiddleware
 from api.mock.path_roles.mock_path_roles import mock_path_roles
-from api.models.database.model import Base, Song
+from api.models.database.model import Song
 from api.mock.database.fill import fill
 from api.utils.routes.extract_role_credentials_from_request import extract_role_credentials_from_request
 from logging_config import setup_logging_testing
@@ -17,85 +16,6 @@ from api.config.database import get_db, get_db_memory
 # setup logging
 setup_logging_testing()
 logger = logging.getLogger("testing")
-
-# setup test db
-engine = create_engine("sqlite:///:memory:", 
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool
-)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# -- DB ENGINE -- #
-@pytest.fixture(scope="session")
-def db_engine():
-    Base.metadata.create_all(bind=engine)
-    yield engine
-
-
-# -- DB SESSIONS -- #
-
-# database = none
-@pytest.fixture(scope="function")
-def db_session_empty(db_engine):
-    """Creates a new database session for a test."""
-    connection = db_engine.connect()
-    transaction = connection.begin()
-    session = TestingSessionLocal(bind=connection)
-    
-    yield session
-    
-    session.close()
-    transaction.rollback()
-    connection.close()
-
-# database = test_model
-@pytest.fixture(scope="function")
-def db_session_filled(db_engine):
-    """Creates a new database session for a test."""
-    connection = db_engine.connect()
-    transaction = connection.begin()
-    session = TestingSessionLocal(bind=connection)
-    fill(session)
-    
-    yield session
-    
-    session.close()
-    transaction.rollback()
-    connection.close()
-
-# -- HTTP CLIENTS -- #
-
-# routes        = none
-# database      = none
-# middleware    no
-@pytest.fixture(scope="function")
-def app_client_empty(): 
-    yield TestClient(FastAPI())
-
-# routes        = prod mirrored
-# database      = test_model
-# middleware    no
-@pytest.fixture(scope="function")
-def app_client_prod_routes(db_session_filled):
-    
-    # simulating prod api
-    app = FastAPI()
-    app.include_router(group_router)
-    app.include_router(song_router)
-    @app.get("/")
-    async def root():
-        return 17
-    
-    def override_get_db():
-        yield db_session_filled
-    
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-    app.dependency_overrides.pop(get_db, None)
-    
-    
-# ----------------------------------------------------NEW------------------------------------------------------------------------------------------
 
 ## -- MAIN FIXTURES -- ## 
 
