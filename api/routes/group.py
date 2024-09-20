1dedbaa82eaee6db7d4b66cb0e5e40f661a8114e
Christian Keller <christiankeller1.student@hs-rm.de>
2024-09-20 18:40:31 +0200

    refactor: import naming

--- api/routes/group.py
+++ api/routes/group.py
@@ -7,14 +7,15 @@ from api.models.schema.group import (DeleteResponse, GetEditsResponse,
                                      GetMembersResponse, GetResponse,
                                      GroupNameResponse, PostRequest,
                                      PostResponse)
-from api.services.database.edit import get_edits_by_group
-from api.services.database.group import create as create_group_service
-from api.services.database.group import get as get_group_service
+from api.services.database.edit import \
+    get_edits_by_group as get_edits_by_group_database
+from api.services.database.group import create as create_group_database
+from api.services.database.group import get as get_group_database
 from api.services.database.group import \
-    list_members as list_members_groud_service
-from api.services.database.group import remove as remove_group_service
-from api.services.database.user import create as create_user_service
-from api.services.database.user import get as get_user_service
+    list_members as list_members_group_database
+from api.services.database.group import remove as remove_group_database
+from api.services.database.user import create as create_user_database
+from api.services.database.user import get as get_user_database
 from api.sessions.database import get_database_session
 from api.utils.jwt.jwt import create_jwt, read_jwt
 
@@ -27,10 +28,10 @@ router = APIRouter(
 @router.post("/", response_model=PostResponse, tags=["group"])
 async def create(request: PostRequest = Body(...), database_session: Session = Depends(get_database_session)): 
     # Erstelle die Gruppe
-    new_group = create_group_service(name=request.groupname, database_session=database_session)
+    new_group = create_group_database(name=request.groupname, database_session=database_session)
 
     # Erstelle einen neuen Benutzer mit den übergebenen Daten
-    new_user = create_user_service(
+    new_user = create_user_database(
         group_id=new_group.group_id,  # group_id der neu erstellten Gruppe verwenden
         role="creator",
         name=request.username,
@@ -50,24 +51,24 @@ async def create(request: PostRequest = Body(...), database_session: Session = D
 @router.delete("/{group_id}", response_model=DeleteResponse, tags=["group"])
 async def delete(group_id: str, database_session: Session = Depends(get_database_session)):
     # Überprüfen, ob die Gruppe existiert und entfernen
-    remove_group_service(group_id, database_session)
+    remove_group_database(group_id, database_session)
 
     return {"message" : "Group successfully deleted"}
 
 @router.get("/{group_id}/name", response_model=GroupNameResponse, tags=["group"])
 async def group_name(group_id: str, database_session: Session = Depends(get_database_session)):
-    group = get_group_service(group_id=group_id, database_session=database_session)
+    group = get_group_database(group_id=group_id, database_session=database_session)
 
     return {"name":group.name}
 
 @router.get("/{group_id}", response_model=GetResponse, tags=["group"])
 async def get(group_id: str, authorization: str = Header(None), database_session: Session = Depends(get_database_session)):
     # group infos
-    group = get_group_service(group_id=group_id, database_session=database_session)
+    group = get_group_database(group_id=group_id, database_session=database_session)
 
     # user infos
     user_id = read_jwt(authorization.replace("Bearer ", ""))
-    user = get_user_service(user_id, database_session=database_session)
+    user = get_user_database(user_id, database_session=database_session)
 
     return {
         "user": {
@@ -82,7 +83,7 @@ async def get(group_id: str, authorization: str = Header(None), database_session
 
 @router.get("/{group_id}/members", response_model=GetMembersResponse, tags=["group"])
 async def get_group_members(group_id: str, database_session: Session = Depends(get_database_session)):
-    members = list_members_groud_service(group_id, database_session=database_session)
+    members = list_members_group_database(group_id, database_session=database_session)
     return {        
         "members": [
             {
@@ -96,7 +97,7 @@ async def get_group_members(group_id: str, database_session: Session = Depends(g
 
 @router.get("/{group_id}/edits", response_model=GetEditsResponse, tags=["group"])
 async def get_group_edits(group_id: str, database_session: Session = Depends(get_database_session)):
-    edits = get_edits_by_group(group_id, database_session=database_session)
+    edits = get_edits_by_group_database(group_id, database_session=database_session)
     return {
         "edits": [
             {
