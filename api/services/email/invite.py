df346bd472a3ef60c40c2a23faa3f5e3c6abfb73
Christian Keller <k.christian9@web.de>
2024-09-26 23:31:04 +0200

    feat: better email text

--- api/services/email/invite.py
+++ api/services/email/invite.py
@@ -2,6 +2,6 @@ from api.sessions.email import BaseEmailSessionManager
 
 
 def invite(to: str, code: str, invite_id: str, groupid: str, email_session: BaseEmailSessionManager) -> bool:
-    return email_session.send(to, "TRITT UNSERER GRUPPE BEI", f"Hier der link zu unserer gruppe. http://localhost:8000/group/{invite_id}/{code}/{groupid}")
+    return email_session.send(to, "TRITT UNSERER GRUPPE BEI", f"Hier der link zu unserer gruppe. http://localhost:3000/{invite_id}/{code}/{groupid}")
 
     
