8016f76d27cc601ee3e8a78800c1206e10f159d8
Christian Keller <k.christian9@web.de>
2024-09-26 22:09:28 +0200

    feat: revert

--- api/routes/static.py
+++ api/routes/static.py
@@ -10,35 +10,35 @@ router = APIRouter(
 
 @router.get("/covers/{filename}", tags=["files"])
 async def serve_covers(filename: str):      
-    file_path = f"files/covers/{filename}"
+    file_path = f"outgoing/files/covers/{filename}"
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return FileResponse(file_path)
 
 @router.get("/demo_slot/{filename}", tags=["files"])
 async def serve_demo_slot(filename: str):   
-    file_path = f"files/demo_slot/{filename}"
+    file_path = f"outgoing/files/demo_slot/{filename}"
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return FileResponse(file_path)
 
 @router.get("/edits/{filename}", tags=["files"])
 async def serve_edits(filename: str):
-    file_path = f"files/edits/{filename}"
+    file_path = f"outgoing/files/edits/{filename}"
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return FileResponse(file_path)
 
 @router.get("/occupied_slots/{filename}", tags=["files"])
 async def serve_occupied_slot(filename: str):
-    file_path = f"files/occupied_slots/{filename}"
+    file_path = f"outgoing/files/occupied_slots/{filename}"
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return FileResponse(file_path)
 
 @router.get("/songs/{filename}", tags=["files"])
 async def serve_songs(filename: str):
-    file_path = f"files/songs/{filename}"
+    file_path = f"outgoing/files/songs/{filename}"
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
     return FileResponse(file_path)
